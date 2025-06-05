9ae8810b22f6d6c81ce02eda17b0f1bb923ff832
Sourav2507 <132735088+Sourav2507@users.noreply.github.com>
2025-06-05 19:50:21 +0530

    New Changes

--- controllers/user.py
+++ controllers/user.py
@@ -1,7 +1,11 @@
 from flask import Blueprint,render_template,redirect,url_for,session
+from werkzeug.security import generate_password_hash,check_password_hash
+from models.customers import *
 
 user = Blueprint('user', __name__)
 
 @user.route('/user')
 def user_db():
-    return render_template('customer_db.html')
+    print(session)
+    user = User.query.filter_by(username=session['username']).first()
+    return render_template('customer_db.html',User1=user)
---
+++ static/js/customer_db.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        User1 : user
+    },
+    methods: {
+        
+    }
+});
